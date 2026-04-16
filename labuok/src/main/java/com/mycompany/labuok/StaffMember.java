0cf65c7aecc67169fb7deaefa1a268e9c468aa44
Anuja Mewan <anujapannipiti@gmail.com>
2026-04-16 12:35:04 +0530

    Update StaffMember.java
    lab04 q1 abstract parent created

---
+++ labuok/src/main/java/com/mycompany/labuok/StaffMember.java
@@ -0,0 +1,38 @@
+package com.mycompany.labuok;
+
+
+public abstract class StaffMember {
+    
+    //add attributes
+    private String fullname;
+    private final String staffId;
+    protected String department;
+    
+    //craete a public constructor
+    public StaffMember(String fullname,String staffId,String department){
+     this.fullname=fullname;
+     this.staffId=staffId;
+     this.department=department;
+    }
+    
+    //create getter methods
+    public String getFullName(){
+     return fullname;
+    }
+    public String getStaffId(){
+     return staffId;
+    }
+    public String getDepartment(){
+     return department;
+    }
+    
+    //create final method
+    public final void displayBasicDetails(){
+     System.out.println("Name: " +fullname+ "\n StaffID: " +staffId+ "\n Department" +department);
+    }
+    
+    //create abstract method
+    public abstract double calculateMonthlyPayment();
+    
+    
+}
