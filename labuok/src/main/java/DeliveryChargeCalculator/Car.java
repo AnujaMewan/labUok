58d38b7daa87b0110a8099b429e7c678a2c830c0
Anuja Mewan <anujapannipiti@gmail.com>
2026-05-08 00:19:34 +0530

    Question 2

---
+++ labuok/src/main/java/DeliveryChargeCalculator/Car.java
@@ -0,0 +1,21 @@
+package DeliveryChargeCalculator;
+
+class Car extends Vehicle {
+
+    private int numberOfDays;
+    private double dailyRate;
+
+    Car(String vehicleNumber, String brand, int numberOfDays, double dailyRate) {
+        super(vehicleNumber, brand);
+        this.numberOfDays = numberOfDays;
+        this.dailyRate = dailyRate;
+    }
+
+    @Override
+    double calculateRentalCost() {
+
+        super.calculateRentalCost();
+
+        return numberOfDays * dailyRate;
+    }
+}
