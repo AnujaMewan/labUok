ea1d4d8451886ad3f337d99cac7be3533dd6c894
Anuja Mewan <anujapannipiti@gmail.com>
2026-06-08 20:01:06 +0530

    do question 1

---
+++ labuok/src/main/java/Question1L8/StudentConnectionTest.java
@@ -0,0 +1,19 @@
+package Question1L8;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class StudentConnectionTest {
+
+    public static void main(String[] args) {
+
+        try (Connection conn =
+                StudentDatabaseConnection.getConnection()) {
+
+            System.out.println("Database connected successfully.");
+
+        } catch (SQLException e) {
+            System.out.println("Database Error: " + e.getMessage());
+        }
+    }
+}
